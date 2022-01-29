5f6d225712f3d1e171fa0db7e4091b98dbeb47d9
Jordana1995 <68743811+Jordana1995@users.noreply.github.com>
2022-01-29 09:51:38 -0300

    POO
    Aula DIO de POO até aqui abstração, encapsulamento e herança

---
+++ Models/Aluno.cs
@@ -0,0 +1,7 @@
+namespace ExemploPOO.Models
+{
+    public class Aluno : Pessoa
+    {
+        public double Nota {get;set;}
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using ExemploPOO.Models;
+
+
+namespace ExemploPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+            // ---------------------------------------------------------- 
+
+            //Retangulo r = new Retangulo();
+
+            //r.Definir(30,30);
+
+            //System.Console.WriteLine($"Área: {r.ObterArea()}");
+
+            //-------------------------------------------------------------
+
+            // Pessoa p1 = new Pessoa();
+
+            // p1.Nome = "Jordana";
+            // p1.Idade = 26;
+
+            // p1.Apresentar();
+
+            //-------------------------------------------------------------
+
+            Aluno aluno1 = new Aluno();
+            aluno1.Nome = "Jessica";
+            aluno1.Idade = 28;
+            aluno1.Nota = 9.5;
+
+            System.Console.WriteLine($"{aluno1.Nome} tem {aluno1.Idade} anos e tirou {aluno1.Nota} de média geral");
+
+            Professor prof = new Professor();
+            prof.Nome = "Leonardo";
+            prof.Idade = 45;
+
+            prof.Apresentar();
+        }
+    }
+}
